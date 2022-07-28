cfc36ab25087804ac8040f4149b172f8497c34b1
Anouar-91 <a.berrouane@hotmail.com>
2022-07-28 11:24:05 +0200

    first commit

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Robot from "../assets/robot.gif";
+export default function Welcome() {
+  const [userName, setUserName] = useState("");
+
+  const loadCurrentUser = async () => {
+    setUserName(
+      await JSON.parse(
+        localStorage.getItem("chat-app_user")
+      ).username
+    );
+  }
+  useEffect( () => {
+    loadCurrentUser();
+  }, []);
+
+  return (
+    <Container>
+      <img src={Robot} alt="" />
+      <h1>
+        Bienvenue, <span>{userName}!</span>
+      </h1>
+      <h3>Choisissez un chat pour démarrer une discussion</h3>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  flex-direction: column;
+  img {
+    height: 20rem;
+  }
+  span {
+    color: #4e0eff;
+  }
+`;
---
+++ src/utils/APIRoutes.js
@@ -0,0 +1,8 @@
+export const host = "http://localhost:3000";
+export const registerRoute = `${host}/api/user/register`;
+export const loginRoute = `${host}/api/user/login`;
+export const setAvatarRoute = `${host}/api/user/avatar`;
+export const allUsersRoute = `${host}/api/user/allusers`;
+export const logoutRoute = `${host}/api/user/allusers`;
+export const sendMessageRoute = `${host}/api/msg/addmsg`;
+export const getAllMessagesRoute = `${host}/api/msg/getmsg`;
